00416c988fdb2395815d8eeecbe0428f01f65d1c
Kaspar Rohrer <krohrer@Tetsuo.local>
2009-01-13 09:36:53 +0100

    Theme for windowing only. OMake.

--- personal/kmr-omake.el
+++ personal/kmr-omake.el
@@ -1,4 +1,15 @@
 (provide 'kmr-omake)
 
-; (setq auto-mode-alist (cons '("OMake\\(file\\|root\\)\\|\\.om" . makefile-bsdmake-mode) auto-mode-alist))
+(require 'make-mode)
 
+(setq auto-mode-alist (cons '("OMake\\(file\\|root\\)\\|\\.om" . kmr-makefile-omake-mode) auto-mode-alist))
+
+(defvar kmr-makefile-omake-mode-syntax-table 
+  (let ((st (copy-syntax-table makefile-mode-syntax-table)))
+    (modify-syntax-entry ?\` "." st)
+    (modify-syntax-entry ?\' "." st))
+  st)
+
+(define-derived-mode kmr-makefile-omake-mode makefile-mode "OMakefile"
+  "An adapted `makefile-mode' that knows about omake."
+  :syntax-table kmr-makefile-omake-mode-syntax-table)
