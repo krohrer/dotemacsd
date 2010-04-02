581371e780c80af54866beacf86ab5f6f35b73a3
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2010-04-02 16:48:50 +0200

    Customization

--- custom.el
+++ custom.el
@@ -4,13 +4,15 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode t)
  '(inhibit-startup-screen t)
  '(mouse-wheel-scroll-amount (quote (1 ((shift) . 5) ((control)))))
  '(paren-match-face (quote paren-face-match-light))
  '(paren-sexp-mode t)
+ '(show-paren-style (quote expression))
  '(time-stamp-format "%U, %3a %3b %2d %02H:%02M:%02S %Z %:y")
  '(truncate-partial-width-windows nil)
- '(tuareg-library-path "/Users/krohrer/CLI/godi/lib/ocaml/std-lib"))
+ '(tuareg-library-path "/opt/local/lib/ocaml"))
 
 ;; Set regular expression rules for syntax coloring
 (custom-set-faces
@@ -18,4 +20,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(cursor ((t (:background "Red" :foreground "white")))))
