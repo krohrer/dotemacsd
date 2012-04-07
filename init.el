33ecff4867d6f4fd3747b60448f67c9d83f40c39
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2012-04-07 13:13:18 -0600

    Removed unused stuff

--- init.el
+++ init.el
@@ -13,19 +13,9 @@
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-;;; (add-to-list 'load-path "/opt/local/share/emacs/site-lisp/w3m")
-;;; (require 'w3m-load)
-
-;(require 'omake-mode)
-;(setq omake-program-path "/opt/local/bin/omake")
-;(setq omake-program-arguments "-P -w --verbose")
-;(setq omake-error-highlight-background "#880000")
-
-
 (add-to-list 'load-path "~/.emacs.d/personal")
 (require 'kmr-generic)
 (require 'kmr-tuareg)
-;;; (require 'kmr-omake)
 (require 'kmr-latex)
 (require 'kmr-color-theme-library)
 (require 'kmr-omake)
