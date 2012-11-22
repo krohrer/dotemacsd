27ce3233ccc822230e7dbf29dd73b14822c9f099
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2012-11-22 09:31:47 +0100

    Removed all OCaml modes.

--- init.el
+++ init.el
@@ -9,13 +9,13 @@
 (require 'color-theme)
 (color-theme-initialize)
 
-(add-to-list 'auto-mode-alist '("\\.ml[iylp]?$" . caml-mode))
-(autoload 'caml-mode "caml" "Major mode for editing OCaml code." t)
-(autoload 'run-caml "inf-caml" "Run an inferior OCaml process." t)
-(autoload 'camldebug "camldebug" "Run ocamldebug on program." t)
-(add-to-list 'interpreter-mode-alist '("ocamlrun" . caml-mode))
-(add-to-list 'interpreter-mode-alist '("ocaml" . caml-mode))
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/caml-mode")
+;(add-to-list 'auto-mode-alist '("\\.ml[iylp]?$" . caml-mode))
+;(autoload 'caml-mode "caml" "Major mode for editing OCaml code." t)
+;(autoload 'run-caml "inf-caml" "Run an inferior OCaml process." t)
+;autoload 'camldebug "camldebug" "Run ocamldebug on program." t)
+;(add-to-list 'interpreter-mode-alist '("ocamlrun" . caml-mode))
+;(add-to-list 'interpreter-mode-alist '("ocaml" . caml-mode))
+;(add-to-list 'load-path "/usr/share/emacs/site-lisp/caml-mode")
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
 
@@ -26,14 +26,22 @@
 (require 'kmr-generic)
 ;(require 'kmr-tuareg)
 (require 'kmr-latex)
-(require 'kmr-color-theme-library)
+;(require 'kmr-color-theme-library)
 (require 'kmr-omake)
 
-(if (and (boundp 'window-system) (not (null window-system)))
-    (kmr-color-theme-chnuschper))
+;(if (and (boundp 'window-system) (not (null window-system)))
+;    (kmr-color-theme-chnuschper))
 
 (if (fboundp 'ns-find-file)
     (global-set-key [ns-drag-file] 'ns-find-file))
 
 (if  (fboundp 'ns-pop-up-frames)
     (ns-pop-up-frames nil))
+
+;(if window-system
+;     (require 'caml-font))
+
+(global-set-key (kbd "<C-tab>") (lambda (&optional arg) "Keyboard macro."
+				  (interactive "p")
+				  (kmacro-exec-ring-item (quote ([17 tab] 0 "%d"))
+							 arg)))
