a0591a9792ef0ebf68b64f1d4987869fdf5a9f1b
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2012-11-22 12:26:15 +0100

    Disabled caml-mode. Added tuareg-mode from https://github.com/emacsmirror/tuareg.git

--- init.el
+++ init.el
@@ -1,32 +1,31 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-;(add-to-list 'load-path "~/.emacs.d/site-lisp/tuareg-mode")
-;(require 'tuareg)
-
 (add-to-list 'load-path "~/.emacs.d/site-lisp/color-theme")
 (require 'color-theme)
 (color-theme-initialize)
 
-(add-to-list 'auto-mode-alist '("\\.ml[iylp]?$" . caml-mode))
-(autoload 'caml-mode "caml" "Major mode for editing OCaml code." t)
-(autoload 'run-caml "inf-caml" "Run an inferior OCaml process." t)
-(autoload 'camldebug "camldebug" "Run ocamldebug on program." t)
-(add-to-list 'interpreter-mode-alist '("ocamlrun" . caml-mode))
-(add-to-list 'interpreter-mode-alist '("ocaml" . caml-mode))
-(add-to-list 'load-path "/opt/local/share/emacs/site-lisp/caml-mode")
+(setq tuareg-use-smie t)
+(load "~/.emacs.d/site-lisp/tuareg/tuareg-site-file")
+
+;(add-to-list 'auto-mode-alist '("\\.ml[iylp]?$" . caml-mode))
+;(autoload 'caml-mode "caml" "Major mode for editing OCaml code." t)
+;(autoload 'run-caml "inf-caml" "Run an inferior OCaml process." t)
+;(autoload 'camldebug "camldebug" "Run ocamldebug on program." t)
+;(add-to-list 'interpreter-mode-alist '("ocamlrun" . caml-mode))
+;(add-to-list 'interpreter-mode-alist '("ocaml" . caml-mode))
+;(add-to-list 'load-path "/opt/local/share/emacs/site-lisp/caml-mode")
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
-
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (add-to-list 'load-path "~/.emacs.d/personal")
 (require 'kmr-generic)
 ;(require 'kmr-tuareg)
-(require 'kmr-latex)
+;(require 'kmr-omake)
+;(require 'kmr-latex)
 (require 'kmr-color-theme-library)
-(require 'kmr-omake)
 
 (if (and (boundp 'window-system)
 	 (not (null window-system)))
