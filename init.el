fd0d2e66d0f45b754897939e12333d2fcde4a9de
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2015-09-14 11:50:55 +0200

    Setup merlin.

--- init.el
+++ init.el
@@ -68,3 +68,22 @@
   (require 'package)
   (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t))
+
+;; Quick merlin setup for EMACS
+;; https://github.com/the-lambda-church/merlin
+
+(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+
+(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+;; Load merlin-mode
+(require 'merlin)
+;; Start merlin on ocaml files
+(add-hook 'tuareg-mode-hook 'merlin-mode t)
+(add-hook 'caml-mode-hook 'merlin-mode t)
+;; Enable auto-complete
+(setq merlin-use-auto-complete-mode 'easy)
+;; Use opam switch to lookup ocamlmerlin binary
+(setq merlin-command 'opam)
+
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+
